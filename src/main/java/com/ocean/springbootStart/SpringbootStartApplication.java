d41fa52633efbb258506ecec76e2ca9454f0c832
cjw <1490114471@@qq.com>
2021-04-28 10:19:03 +0800

    #常用依赖#

---
+++ src/main/java/com/ocean/springbootStart/SpringbootStartApplication.java
@@ -0,0 +1,13 @@
+package com.ocean.springbootStart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStartApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ocean/springbootStart/config/CorsConfig.java
@@ -0,0 +1,36 @@
+package com.ocean.springbootStart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author cjw
+ */
+@Configuration
+public class CorsConfig {
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //允许任何域名
+        corsConfiguration.addAllowedOrigin("*");
+        //允许任何头
+        corsConfiguration.addAllowedHeader("*");
+        //允许任何方法
+        corsConfiguration.addAllowedMethod("*");
+        // 为了跨域cookie的共享
+        //corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setMaxAge(3600L);
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        //注册
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
